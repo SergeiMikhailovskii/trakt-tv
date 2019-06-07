35efdb8b619dcbfd7b0cfc6b1e58cf175b6a9979
siarhei.mikhaylouski <35fulofe>
2019-06-07 09:26:52 +0300

    Changed RX logic

--- app/src/main/java/com/mikhailovskii/trakttv/ui/movie_detail/MovieDetailPresenter.java
+++ app/src/main/java/com/mikhailovskii/trakttv/ui/movie_detail/MovieDetailPresenter.java
@@ -1,58 +1,60 @@
 package com.mikhailovskii.trakttv.ui.movie_detail;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.mikhailovskii.trakttv.data.api.MovieAPIFactory;
 import com.mikhailovskii.trakttv.data.entity.Movie;
 import com.mikhailovskii.trakttv.ui.base.BasePresenter;
 
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MovieDetailPresenter extends BasePresenter<MovieDetailContract.MovieDetailView>
         implements MovieDetailContract.MovieDetailPresenter {
 
+    private CompositeDisposable mCompositeDisposable;
+
+    @Override
+    public void attachView(@NonNull MovieDetailContract.MovieDetailView movieDetailView) {
+        super.attachView(movieDetailView);
+        mCompositeDisposable = new CompositeDisposable();
+    }
+
+    @Override
+    public void detachView() {
+        super.detachView();
+        mCompositeDisposable.clear();
+    }
+
     @Override
     public void loadMovieDetails(@Nullable String slugId) {
 
         if (slugId != null) {
-            MovieAPIFactory.getInstance().getAPIService()
-                    .getExtendedInfo(slugId)
+            view.showLoadingIndicator(true);
+            final Movie[] movie = new Movie[1];
+
+            view.showLoadingIndicator(true);
+
+            mCompositeDisposable.add(MovieAPIFactory.getInstance().getAPIService().getExtendedInfo(slugId)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(new Observer<Movie>() {
-
-                        Movie movie = null;
-
-                        @Override
-                        public void onSubscribe(Disposable d) {
-                            view.showLoadingIndicator(true);
-                        }
-
-                        @Override
-                        public void onNext(Movie movieRes) {
-                            movie = movieRes;
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-                            view.showEmptyState(true);
-                            view.onMovieDetailsFailed();
-                        }
-
-                        @Override
-                        public void onComplete() {
-                            view.showLoadingIndicator(false);
-                            if (movie != null) {
-                                view.showEmptyState(false);
-                                view.onMovieDetailsLoaded(movie);
-                            } else {
+                    .subscribe(
+                            //onNext
+                            movieRes -> movie[0] = movieRes,
+                            //onError
+                            throwable -> {
                                 view.showEmptyState(true);
+                                view.onMovieDetailsFailed();
+                            },
+                            //onCompleted
+                            () -> {
+                                view.showLoadingIndicator(false);
+                                view.showEmptyState(false);
+                                view.onMovieDetailsLoaded(movie[0]);
                             }
-                        }
-                    });
+                    ));
         }
 
     }
--- app/src/main/java/com/mikhailovskii/trakttv/ui/movies_list/MovieListPresenter.java
+++ app/src/main/java/com/mikhailovskii/trakttv/ui/movies_list/MovieListPresenter.java
@@ -1,5 +1,7 @@
 package com.mikhailovskii.trakttv.ui.movies_list;
 
+import android.support.annotation.NonNull;
+
 import com.mikhailovskii.trakttv.data.api.MovieAPIFactory;
 import com.mikhailovskii.trakttv.data.entity.Movie;
 import com.mikhailovskii.trakttv.data.entity.MovieTrack;
@@ -8,61 +10,68 @@ import com.mikhailovskii.trakttv.ui.base.BasePresenter;
 import java.util.ArrayList;
 import java.util.List;
 
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MovieListPresenter extends BasePresenter<MovieListContract.MoviesListView>
         implements MovieListContract.MoviesListPresenter {
 
     private static final String IMG_URL = "https://cdn4.iconfinder.com/data/icons/photo-video-outline/100/objects-17-512.png";
+    private CompositeDisposable mCompositeDisposable;
+
+    @Override
+    public void attachView(@NonNull MovieListContract.MoviesListView moviesListView) {
+        super.attachView(moviesListView);
+        mCompositeDisposable = new CompositeDisposable();
+
+    }
+
+    @Override
+    public void detachView() {
+        super.detachView();
+        mCompositeDisposable.clear();
+    }
 
     @Override
     public void loadMovieList() {
-        MovieAPIFactory.getInstance().getAPIService()
-                .getMovies()
+        List<Movie> list = new ArrayList<>();
+
+        view.showLoadingIndicator(true);
+        mCompositeDisposable.add(MovieAPIFactory.getInstance().getAPIService().getMovies()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<List<MovieTrack>>() {
-
-                    List<Movie> list = new ArrayList<>();
-
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                        view.showLoadingIndicator(true);
-                    }
-
-                    @Override
-                    public void onNext(List<MovieTrack> movieTracks) {
-                        for (MovieTrack movieTrack : movieTracks) {
-                            Movie movie = new Movie(IMG_URL,
-                                    movieTrack.movie.getName(),
-                                    movieTrack.movie.getYear(),
-                                    movieTrack.movie.movieId.getSlug(),
-                                    movieTrack.getWatchersNumber());
-                            list.add(movie);
-                        }
-                    }
-
-                    @Override
-                    public void onError(Throwable exception) {
-                        view.showEmptyState(true);
-                        view.onMovieListFailed();
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        view.showLoadingIndicator(false);
-                        if (!list.isEmpty()) {
-                            view.showEmptyState(false);
-                            view.onMovieListLoaded(list);
-                        } else {
+                .subscribe(
+                        //onNext
+                        movieTracks -> {
+                            for (MovieTrack movieTrack : movieTracks) {
+                                Movie movie = new Movie(IMG_URL,
+                                        movieTrack.movie.getName(),
+                                        movieTrack.movie.getYear(),
+                                        movieTrack.movie.movieId.getSlug(),
+                                        movieTrack.getWatchersNumber());
+                                list.add(movie);
+                            }
+                        },
+                        //onError
+                        throwable -> {
                             view.showEmptyState(true);
+                            view.onMovieListFailed();
+                        },
+                        //onCompleted
+                        () -> {
+                            view.showLoadingIndicator(false);
+                            if (!list.isEmpty()) {
+                                view.showEmptyState(false);
+                                view.onMovieListLoaded(list);
+                            } else {
+                                view.showEmptyState(true);
+                            }
                         }
-                    }
-                });
+
+                ));
 
     }
+
 
 }
